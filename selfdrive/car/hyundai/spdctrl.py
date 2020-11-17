d4e78dccc4ef0b6bfe7094e70d47b8b44e38a1ba
Vehicle Researcher <>
2020-11-18 07:47:52 +0900

    openpilot v0.8.0 release

--- selfdrive/car/hyundai/spdctrl.py
+++ selfdrive/car/hyundai/spdctrl.py
@@ -152,7 +152,7 @@ class Spdctrl(SpdController):
             elif CS.clu_Vanz > 40 and lead_objspd < 0 and (int(CS.clu_Vanz)-1) <= int(CS.VSetDis) and int(CS.clu_Vanz) >= dRel*2.1 and 1 < dRel < 149: # 유지거리 범위 외 감속 조건 앞차 감속중 현재속도/2 아래로 거리 좁혀졌을 때 상대속도에 따라 점진적 감소
                 self.seq_step_debug = "SS>VS,v>40,-1"
                 lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, max(15, 60+(lead_objspd*2)), -1)
-            elif (0 <= int(CS.clu_Vanz) <= 1 or 7 < int(CS.clu_Vanz) < 30) and lead_objspd < 0 and CS.VSetDis > 30:
+            elif 7 < int(CS.clu_Vanz) < 30 and lead_objspd < 0 and CS.VSetDis > 30:
                 self.seq_step_debug = "SS>VS,30이하"
                 lead_wait_cmd, lead_set_speed = self.get_tm_speed( CS, 15, -5)
             elif lead_objspd == 0 and int(CS.clu_Vanz)+10 <= int(CS.VSetDis) and int(CS.clu_Vanz) > 40 and 1 < dRel < 149: # 앞차와 속도 같을 시 현재속도+10으로 크루즈설정속도 유지
