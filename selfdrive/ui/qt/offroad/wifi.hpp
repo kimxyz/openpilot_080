689ce7b40605d5f3456ed6ac4134458c64d05b38
Vehicle Researcher <>
2020-11-21 18:08:03 +0900

    openpilot v0.8.0 release

--- selfdrive/ui/qt/offroad/wifi.hpp
+++ selfdrive/ui/qt/offroad/wifi.hpp
@@ -1,33 +1,38 @@
 #pragma once
-#include "wifiManager.hpp"
+
 #include <QWidget>
-#include <QtDBus>
-#include <QPushButton>
 #include <QButtonGroup>
 #include <QVBoxLayout>
-#include <QStackedLayout>
+#include <QStackedWidget>
 #include <QTimer>
 
+#include "wifiManager.hpp"
+#include "input_field.hpp"
 
-class CustomConnectButton : public QPushButton{
-
-public:
-    explicit CustomConnectButton(QString text, int iid);
-    int id;
-};
 
 class WifiUI : public QWidget {
   Q_OBJECT
 
-  private:
-    WifiManager* wifi;
-    QVBoxLayout* vlayout;
-    QTimer * timer;
+private:
+  WifiManager* wifi;
 
-  public:
-    explicit WifiUI(QWidget *parent = 0);
+  QStackedWidget* swidget;
+  QVBoxLayout* vlayout;
+  QWidget * wifi_widget;
 
-  private slots:
-    void handleButton(QAbstractButton* m_button);
-    void refresh();
+  InputField *a;
+  QEventLoop loop;
+  QTimer * timer;
+  QString text;
+  QButtonGroup *connectButtons;
+
+  QString getStringFromUser();
+
+public:
+  explicit WifiUI(QWidget *parent = 0);
+
+private slots:
+  void handleButton(QAbstractButton* m_button);
+  void refresh();
+  void receiveText(QString text);
 };
