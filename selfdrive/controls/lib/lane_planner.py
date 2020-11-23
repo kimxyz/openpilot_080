c7804b96e9ab6ef0a50a5b801f82aa35fa0be2d0
Vehicle Researcher <>
2020-11-24 07:27:04 +0900

    openpilot v0.8.0 release

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -30,9 +30,9 @@ class LanePlanner:
     self.p_poly = [0., 0., 0., 0.]
     self.d_poly = [0., 0., 0., 0.]
 
-    self.lane_width_estimate = 3.5
+    self.lane_width_estimate = 3.7
     self.lane_width_certainty = 1.0
-    self.lane_width = 3.5
+    self.lane_width = 3.7
 
     self.l_prob = 0.
     self.r_prob = 0.
@@ -139,11 +139,11 @@ class LanePlanner:
     self.lane_width_certainty += 0.05 * (l_prob * r_prob - self.lane_width_certainty)
     current_lane_width = abs(self.l_poly[3] - self.r_poly[3])
     self.lane_width_estimate += 0.005 * (current_lane_width - self.lane_width_estimate)
-    speed_lane_width = interp(v_ego, [0., 31.], [2.7, 3.6])
+    speed_lane_width = interp(v_ego, [0., 31.], [2.8, 3.5])
     self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
                       (1 - self.lane_width_certainty) * speed_lane_width
 
-    clipped_lane_width = min(3.8, self.lane_width)
+    clipped_lane_width = min(4.0, self.lane_width)
     path_from_left_lane = self.l_poly.copy()
     path_from_left_lane[3] -= clipped_lane_width / 2.0
     path_from_right_lane = self.r_poly.copy()
