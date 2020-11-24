431c3abe4c6c39469e2f5367c89f6c605ae675a1
Vehicle Researcher <>
2020-11-25 01:18:22 +0900

    openpilot v0.8.0 release

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -54,13 +54,13 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalTuning.kpBP = [0., 1.38, 4.17, 8.33, 16.67, 22.22, 27.78] # 0km/h, 5km/h, 15km/h, 30km/h, 60km/h, 80km/h 100km/h
     ret.longitudinalTuning.kpV = [0.1, 1.4, 1.3, 1.2, 0.75, 0.7, 0.65]
     ret.longitudinalTuning.kiBP = [0., 1.38, 4.17, 8.33, 16.67, 22.22, 27.78] # 0km/h, 5km/h, 15km/h, 30km/h, 60km/h, 80km/h 100km/h
-    ret.longitudinalTuning.kiV = [0.5, 0.7, 0.9, 1.2, 0.8, 0.5, 0.5]
+    ret.longitudinalTuning.kiV = [1.5, 1.5, 1.5, 1.5, 1.5, 1.0, 1.0]
     ret.longitudinalTuning.deadzoneBP = [0.0, 0.5]
     ret.longitudinalTuning.deadzoneV = [0.00, 0.00]
     ret.gasMaxBP = [0., 1.38, 4.17, 8.33, 16.67, 22.22, 27.78] # 0km/h, 5km/h, 15km/h, 30km/h, 60km/h, 80km/h 100km/h
     ret.gasMaxV = [2.0, 2.1, 2.2, 2.3, 1.6, 1.5, 1.4]
     ret.brakeMaxBP = [0., 1.38, 4.17, 8.33, 16.67, 22.22, 27.78] # 0km/h, 5km/h, 15km/h, 30km/h, 60km/h, 80km/h 100km/h
-    ret.brakeMaxV = [3.5, 4, 4.5, 5.5, 5, 4, 4]  # safety limits to stop unintended deceleration
+    ret.brakeMaxV = [3.5, 3.5, 3.5, 3.5, 3.5, 3.5, 3.5]  # safety limits to stop unintended deceleration
     ret.steerMaxV = [LqrSteerMaxV]
     ret.steerMaxBP = [0.]
 
