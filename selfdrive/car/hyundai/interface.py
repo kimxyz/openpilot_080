afd78c44f3ba356d6db676affb1498a5f07d31b0
Vehicle Researcher <>
2020-11-24 12:18:07 +0900

    openpilot v0.8.0 release

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from cereal import car
-from common.op_params import opParams
 from common.params import Params
 from selfdrive.config import Conversions as CV
 from selfdrive.car.hyundai.values import Ecu, ECU_FINGERPRINT, CAR, FINGERPRINTS, Buttons
