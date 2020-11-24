5566bdff8e896e2bf4646e0af557e85813d4c2ca
Vehicle Researcher <>
2020-11-24 16:42:18 +0900

    openpilot v0.8.0 release

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -303,11 +303,11 @@ class CarInterface(CarInterfaceBase):
     self.CS.out = ret.as_reader()
     return self.CS.out
 
-  def apply(self, c):
+  def apply(self, c, sm):
     can_sends = self.CC.update(c.enabled, self.CS, self.frame, c.actuators,
                                c.cruiseControl.cancel, c.hudControl.visualAlert, c.hudControl.leftLaneVisible,
                                c.hudControl.rightLaneVisible, c.hudControl.leftLaneDepart, c.hudControl.rightLaneDepart,
                                c.hudControl.setSpeed, c.hudControl.leadVisible, c.hudControl.leadDistance,
-                               c.hudControl.leadvRel, c.hudControl.leadyRel)
+                               c.hudControl.leadvRel, c.hudControl.leadyRel, sm)
     self.frame += 1
     return can_sends
