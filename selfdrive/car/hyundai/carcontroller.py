7655976ee8621555ca80e02f85c5161915067352
Vehicle Researcher <>
2020-11-25 14:55:44 +0900

    openpilot v0.8.0 release

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -241,28 +241,12 @@ class CarController():
     elif (CS.cancel_button_count == 3) and self.cp_oplongcontrol:
       self.usestockscc = not self.usestockscc
 
-    #if not self.usestockscc:
-    #  self.gapcount += 1
-    #  if self.gapcount == 50 and self.gapsettingdance == 2:
-    #    self.gapsettingdance = 1
-    #    self.gapcount = 0
-    #  elif self.gapcount == 50 and self.gapsettingdance == 1:
-    #    self.gapsettingdance = 4
-    #    self.gapcount = 0
-    #  elif self.gapcount == 50 and self.gapsettingdance == 4:
-    #    self.gapsettingdance = 3
-    #    self.gapcount = 0
-    #  elif self.gapcount == 50 and self.gapsettingdance == 3:
-    #    self.gapsettingdance = 2
-    #    self.gapcount = 0
-
     if self.prev_gapButton != CS.cruise_buttons:  # gap change.
       if CS.cruise_buttons == 3:
         self.gapsettingdance -= 1
       if self.gapsettingdance < 1:
         self.gapsettingdance = 4
       self.prev_gapButton = CS.cruise_buttons
-    print('gap={} cnt={}'.format(self.gapsettingdance, self.prev_gapButton))
 
     self.apply_steer_last = apply_steer
 
