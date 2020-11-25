dca0739f75d61e61b194500f58d59adf48e7a00a
Vehicle Researcher <>
2020-11-26 05:39:29 +0900

    openpilot v0.8.0 release

--- selfdrive/controls/lib/latcontrol_pid.py
+++ selfdrive/controls/lib/latcontrol_pid.py
@@ -3,7 +3,7 @@ from selfdrive.controls.lib.drive_helpers import get_steer_max
 from cereal import car
 from cereal import log
 from common.params import Params
-
+from common.numpy_fast import interp
 
 class LatControlPID():
   def __init__(self, CP):
@@ -12,6 +12,9 @@ class LatControlPID():
                             k_f=CP.lateralTuning.pid.kf, pos_limit=1.0, neg_limit=-1.0,
                             sat_limit=CP.steerLimitTimer)
     self.angle_steers_des = 0.
+    self.speed_range = [0, 17]
+    self.steer_feedforward_param = [1.5, 2]
+    self.steer_feedforward_value = 2
 
   def reset(self):
     self.pid.reset()
@@ -35,7 +38,8 @@ class LatControlPID():
       if CP.steerControlType == car.CarParams.SteerControlType.torque:
         # TODO: feedforward something based on path_plan.rateSteers
         steer_feedforward -= path_plan.angleOffset   # subtract the offset, since it does not contribute to resistive torque
-        steer_feedforward *= CS.vEgo**2  # proportional to realigning tire momentum (~ lateral accel)
+        self.steer_feedforward_value = interp(CS.vEgo, self.speed_range, self.steer_feedforward_param)
+        steer_feedforward *= CS.vEgo**self.steer_feedforward_value  # proportional to realigning tire momentum (~ lateral accel)
       deadzone = int(Params().get('IgnoreZone')) * 0.1
 
       check_saturation = (CS.vEgo > 10) and not CS.steeringRateLimited and not CS.steeringPressed
