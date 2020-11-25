4ca63c40cd438afbd60d2227408326f7ce8f905b
Vehicle Researcher <>
2020-11-25 13:59:38 +0900

    openpilot v0.8.0 release

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -524,7 +524,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
     ET.WARNING: Alert(
       "",
       "",
-      AlertStatus.normal, AlertSize.none,
+      AlertStatus.normal, AlertSize.small,
       Priority.LOW, VisualAlert.none, AudibleAlert.none, .1, .1, .1),
   },
 
