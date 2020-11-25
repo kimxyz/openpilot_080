73c45aa4f166523da842036120fe57fbfb9df6fe
Vehicle Researcher <>
2020-11-26 02:50:29 +0900

    Revert "openpilot v0.8.0 release"
    This reverts commit 5b28e6e152dc7cd6ffc2cae8198276b298e659eb.

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -10,7 +10,7 @@ STOPPING_TARGET_SPEED = MIN_CAN_SPEED + 0.01
 STARTING_TARGET_SPEED = 0.5
 BRAKE_THRESHOLD_TO_PID = 0.2
 
-STOPPING_BRAKE_RATE = 4.5  # brake_travel/s while trying to stop
+STOPPING_BRAKE_RATE = 0.2  # brake_travel/s while trying to stop
 STARTING_BRAKE_RATE = 0.8  # brake_travel/s while releasing on restart
 BRAKE_STOPPING_TARGET = 0.5  # apply at least this amount of brake to maintain the vehicle stationary
 
